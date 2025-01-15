32faaa4848c217024b43618e8da71920fdb3856e
Kianoush <kianoush.amirpour@gmail.com>
2025-01-15 11:53:02 +0330

    fix flake8 issues

--- deployment/serving_api/app/main.py
+++ deployment/serving_api/app/main.py
@@ -12,4 +12,3 @@ app.include_router(api_router, prefix=settings.API_V1_STR)
 if __name__ == "__main__":
     import uvicorn  # type: ignore
     uvicorn.run("app.main:app", host="localhost", port=8000, reload=True)
-    
